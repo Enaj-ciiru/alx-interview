1a78ddc80fa9f3e539946153814b95fc1723c51e
Enaj-ciiru <jnjoroge.immaculate@gmail.com>
2023-02-23 18:12:20 +0300

    change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-"""Module Making Change"""
+"""
+Change comes from within
+"""
 
 
 def makeChange(coins, total):
-    """Return: fewest number of coins needed
-    to meet total"""
-    if (type(total) is not int or type(coins) is not list):
-        return -1
-
+    """
+    Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet a given amount total
+    """
     if total <= 0:
         return 0
-
-    try:
-        Min = [float('inf') for i in range(total+1)]
-        Min[0] = 0
-        for i in range(1, total+1):
-            for j in range(len(coins)):
-                if Min[i - coins[j]] + 1 < Min[i]:
-                    Min[i] = Min[i - coins[j]] + 1
-
-        if Min[total] != float('inf'):
-            return Min[total]
-        else:
-            return -1
-    except Exception:
-        return -1
+    dp = [0] + [float("inf")] * (total)
+    for coin in coins:
+        for i in range(coin, total + 1):
+            dp[i] = min(dp[i], dp[i - coin] + 1)
+    return dp[-1] if dp[-1] != float("inf") else -1
